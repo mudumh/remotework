b8b4c2e82994e09e97a0aa636d8d827585bf3130
harsha <harsha.jaigurudev@gmail.com>
2014-11-11 11:03:45 -0500

    trying to fix the bug in call

--- lib/stripe_events/new_paid_post.rb
+++ lib/stripe_events/new_paid_post.rb
@@ -3,8 +3,9 @@ class NewPaidPost
   def call(event)
    
     #logger.info "NEW PAID POST"
-    data_json = JSON.parse request.body.read
-    temp_job_id = data_json['data']['object']['metadata']['job_id']
+    puts "the event is"
+    puts event
+    temp_job_id = event['data']['object']['metadata']['job_id']
     #logger.info "The cookie value is #{temp_job_id}"
     @temp_job = TempJobPost.find_by(id: temp_job_id)
     attributes = @temp_job.attributes.except!("id")

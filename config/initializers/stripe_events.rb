2f4d87bdd6db3e1c6af9a66ec37123dbcfdb65e2
harsha <harsha.jaigurudev@gmail.com>
2014-11-11 10:09:03 -0500

    using require newpaidpost

--- config/initializers/stripe_events.rb
+++ config/initializers/stripe_events.rb
@@ -1,6 +1,6 @@
 # Do I need this ?
 #Stripe.api_key = ENV['STRIPE_API_KEY'] # Set your api key
-
+require 'new_paid_post'
 StripeEvent.configure do |events|
   events.subscribe 'charge.succeeded', NewPaidPost.new
 end

798f55b532d2aca1e8ef02bcd241fa7ae86c73d7
harsha <harsha.jaigurudev@gmail.com>
2014-11-11 10:50:10 -0500

    bug fix

--- lib/stripe_events/new_paid_post.rb
+++ lib/stripe_events/new_paid_post.rb
@@ -2,9 +2,9 @@ class NewPaidPost
   
   def call(event)
    
-    logger.info "NEW PAID POST"
+    #logger.info "NEW PAID POST"
     temp_job_id = cookies[:temp_job_id]
-    logger.info "The cookie value is #{temp_job_id}"
+    #logger.info "The cookie value is #{temp_job_id}"
     @temp_job = TempJobPost.find_by(id: temp_job_id)
     attributes = @temp_job.attributes.except!("id")
     JobPosting.create(attributes)

ea989aa008e6f5aaf82ccdbe144b4ddce097deb0
harsha <harsha.jaigurudev@gmail.com>
2014-11-25 12:59:11 -0500

    minor changes to the color scheme in the index page

--- app/helpers/job_postings_helper.rb
+++ app/helpers/job_postings_helper.rb
@@ -1,7 +1,7 @@
 module JobPostingsHelper
 
   def jobpostcompany(job_posting)
-    "<span id= 'companytitle'>#{job_posting.company.name}</span> ".html_safe
+    "<span id= 'companytitle'> at #{job_posting.company.name}</span> ".html_safe
   end
    def jobpost(job_posting)
     "<span id='jobtitle'> #{job_posting.job_title} </span> &nbsp;".html_safe
